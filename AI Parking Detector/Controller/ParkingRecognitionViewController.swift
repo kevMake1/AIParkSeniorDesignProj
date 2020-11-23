999f0409509e14b84aa10ca7e4593f5e73864926
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-11-23 16:07:23 -0500

    fixes

--- AI Parking Detector/Controller/ParkingRecognitionViewController.swift
+++ AI Parking Detector/Controller/ParkingRecognitionViewController.swift
@@ -21,7 +21,7 @@ class ParkingRecognitionViewController: CameraViewController {
         // Setup Vision parts
         let error: NSError! = nil
         
-        guard let modelURL = Bundle.main.url(forResource: "groundview", withExtension: "mlmodelc") else {
+        guard let modelURL = Bundle.main.url(forResource: "GroundView", withExtension: "mlmodelc") else {
             return NSError(domain: "VisionObjectRecognitionViewController", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
         }
         do {
