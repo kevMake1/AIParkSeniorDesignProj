a4fb67f57a3ef89fabf1acec9ad4988fbf8aa276
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-11-26 21:13:40 -0500

    updates

--- AI Parking Detector/Controller/HomeViewController.swift
+++ AI Parking Detector/Controller/HomeViewController.swift
@@ -39,7 +39,7 @@ class HomeViewController: UIViewController {
         self.performSegue(withIdentifier: "groundView", sender: self)
     }
     
-    
+    //cameraView
     @IBAction func cameraBtnPressed(_ sender: UIButton) {
         self.performSegue(withIdentifier: "CameraView", sender: self)
     }
