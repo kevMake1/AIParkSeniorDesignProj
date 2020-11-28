8e84137ae139136f94fad4a1b568f93773dbb0ee
Kevaughn Clarke <k.clarke0894@gmail.com>
2020-11-28 02:01:39 -0500

    update

--- AI Parking Detector/Controller/HomeViewController.swift
+++ AI Parking Detector/Controller/HomeViewController.swift
@@ -19,7 +19,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
     
 //MARK:- Navigation
